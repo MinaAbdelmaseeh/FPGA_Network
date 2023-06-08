9fb01432516cc6f2aff54178c3eede73ed9ded7d
MinaAbdelmaseeh <118192329+MinaAbdelmaseeh@users.noreply.github.com>
2023-06-08 22:30:10 +0300

    updated files

--- VGA_Controller.vhd
+++ VGA_Controller.vhd
@@ -12,14 +12,27 @@ entity VGA_Controller is
            Vsync : out STD_LOGIC :='0';
            clk_out : out std_logic;
            disp_flag : out std_logic;
-           x, y : out integer := 1
+           x, y,ram_index,col,row : out integer := 0
            ); -- initialized to 1
 end VGA_Controller;
 
 
 
 architecture Behavioral of VGA_Controller is
+constant CHAR_SPACING : integer := 2;
+constant LINE_SPACING : integer := 4;
+constant CHAR_WIDTH : integer := 8;
+constant CHAR_HEIGHT : integer := 16;
+constant DISPLAY_LEFT_MARGIN : integer := 0;
+constant DISPLAY_RIGHT_MARGIN : integer := 0;
+constant DISPLAY_TOP_MARGIN : integer := 0;
+constant DISPLAY_BOTTOM_MARGIN : integer := 0;
+constant DISPLAY_WIDTH : integer:= 640;
+constant DISPLAY_HEIGHT : integer := 480;
 
+constant CHARS_PER_ROW :integer := (DISPLAY_WIDTH)/(CHAR_WIDTH + CHAR_SPACING);
+constant NUM_LINES :integer := (DISPLAY_HEIGHT)/(CHAR_HEIGHT + LINE_SPACING);
+constant MAX_CHARS : integer :=CHARS_PER_ROW*NUM_LINES;
     signal clk_50,clk_25: std_logic :='0';
     constant T_FP_H: integer :=16;
     constant T_BP_H: integer :=48;
@@ -35,10 +48,14 @@ architecture Behavioral of VGA_Controller is
      
     signal display_start_v,display_start_h : STD_LOGIC :='0';
     signal signal_v : STD_LOGIC;
-    signal xInt, yInt : integer := 0;
+    signal xInt, yInt,xpos,ypos,pos,count_x,count_y : integer := 0;
     
 begin
-
+--x_counter<=count_x;
+--y_counter<=count_y;
+ram_index<=count_x + count_y * CHARS_PER_ROW;
+col<=xpos;
+row<=ypos;
 disp_flag<= display_start_h and display_start_v;
 clk_div:
         process(clk,clk_50)
@@ -77,13 +94,43 @@ H_sync_generation:
 process(clk_25, display_start_v, display_start_h) begin
     if(rising_edge(clk_25)) then 
         if(display_start_h='1' and display_start_v='1') then
-             xInt<=(xInt +1);
+            if(xInt + 1 = 640) then
+                xInt<=0;
+             else
+                xInt<=xInt + 1;
+             end if;
+             if(xpos + 1 =CHAR_WIDTH+CHAR_SPACING) then
+                 if(count_x+1= CHARS_PER_ROW) then
+                    count_x<=0;
+                 else
+                    count_x<=count_x+1;
+                 end if;
+                 xpos<=0;
+             else
+                 xpos<= xpos + 1;
+             end if;
         else
             xInt<=0;
         end if;
     end if;
     if(rising_edge(display_start_h) and display_start_v='1') then
-        yInt<= (yInt + 1) mod 480;
+        if(yInt + 1 =480) then
+            yInt<=0;
+        else
+            yInt<= yInt + 1;
+        end if;
+        if(ypos + 1 =CHAR_HEIGHT+LINE_SPACING) then
+            ypos<=0;
+            if(count_y + 1 = NUM_LINES) then
+                count_y<=0;
+            else
+                count_y<=count_y+1;
+            end if;
+
+        else
+            ypos<= ypos + 1;
+        end if;
+        --yInt<= (yInt + 1) mod 480;
     end if;
 --    if(rising_edge(display_start_v)) then
 --        yInt<=0;
